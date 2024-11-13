1ad9c99570b34f02f8e863556856b981a1ad6843
tonyfalcondevweb <tonyfalcondevweb@gmail.com>
2024-11-13 18:55:28 +0100

    feat verceljson: ajoute de chemin pour les routes react router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,9 +6,7 @@ import Result from "./Components/Result";
 import PublicRoutes from "./Routes/PublicRoutes";
 import NotFound from "./Components/NotFound";
 
-
 function App() {
-
   return (
     <SpotifyProvider>
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
@@ -16,16 +14,16 @@ function App() {
           <Route element={<SpotifyAccessRoutes />}>
             <Route path="/top10" element={<Result />} />
           </Route>
-          
+
           <Route element={<PublicRoutes />}>
-          <Route path="/" element={<LoginSpotify />} />
+            <Route path="/" element={<LoginSpotify />} />
           </Route>
-          
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </SpotifyProvider>
-  )
+  );
 }
 
-export default App
+export default App;
